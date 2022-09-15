89f9825c91a9f9fd958c8b93670287378625f0cb
masonchain <masonlandrystacker@gmail.com>
2022-09-15 15:59:31 -0500

    Badge struct -- break time

--- contracts/contracts/Badger V2/Sash/BadgerSash.sol
+++ contracts/contracts/Badger V2/Sash/BadgerSash.sol
@@ -9,15 +9,32 @@ contract BadgerSash is
       ERC1155
     , Initializable
 { 
+    struct Badge { 
+        bool permanent;
+        string uri;
+        uint256 maxSupply;
+        uint256 totalSupply;
+        uint256 maxPerSash;
+    }
+
+    /// @dev Keep track of who the absolute leader is.
     address public leader;
 
-    uint256 totalSupply;
+    Badge[] public badges;
 
     constructor(
         string memory _uri
     ) 
         ERC1155(_uri)
-    {}
+    {
+        
+    }
+
+
+    modifier onlyLeader() {
+        require(msg.sender == leader, "Only leader can mint");
+        _;
+    }
 
     function initialize(
         address _leader
@@ -28,28 +45,60 @@ contract BadgerSash is
         leader = _leader;
     }
 
-    /**
-     * @notice Enables the minting of a sash within an organization.
-     */
-    function mint() 
-        external 
+    function sewBadge(
+          Badge calldata _badge
+        , uint256 _badgeId
+    )
+        public
+        virtual
+        onlyLeader()
     {
-        _mint(
-              _msgSender()
-            , totalSupply++
-            , 1
-            , "0x"
+        require(
+              _msgSender() == leader
+            , "BadgerSash::sewBadge: Only the leader can sew badges."
         );
 
-        
+        Badge storage badge = badges[_badgeId];
+
+        /// @dev Confirm that the badge id being sewn is valid.
+        require(
+              !badge.permanent || 
+              badge.totalSupply + badge.maxSupply == 0
+            , "BadgerSash::sewBadge: Badge already exists."
+        );
+
+        /// @dev Send the badge off to the press!        
+        badges[_badgeId] = _badge;
+    }
+
+    function mint(
+          uint256 _badgeId
+        , address _to
+        , uint256 _amount
+        , bytes memory _data
+    ) 
+        public
+        virtual
+    {
+        Badge storage badge = badges[_badgeId];
+
+        // TODO: Import from toolbox Dynamic Qualifers
+
+        _mint(
+            _to,
+            _badgeId,
+            _amount,
+            _data 
+        );
     }
 
     function burn(
         uint256[] memory ids,
         uint256[] memory amounts
     )
-        external
+        public
+        virtual
     {
-        // Heirarchy stuff
+        
     }
 }
