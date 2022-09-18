52b5105ca6b43434f0e748796657b6b2bb1f7233
nftchance <chance@utc24.io>
2022-09-18 13:49:42 -0500

    feat: frontend: connected wallet dom

--- frontend/src/components/Dashboard/Sidebar/OrgSidebar.js
+++ frontend/src/components/Dashboard/Sidebar/OrgSidebar.js
@@ -5,16 +5,28 @@ import Logout from "./Logout/Logout";
 import "../../../style/Dashboard/Sidebar/OrgSidebar.css";
 
 const OrgSidebar = ({ organizations }) => {
-    console.log(organizations);
+
+    const account = {
+        monocre: "nftchance.eth",
+        avatar: "https://avatars.githubusercontent.com/u/77760087?s=200&v=4"
+    }
 
     return (
         <div className="sidebar left">
+            {/* Logged in user header */}
+            <div className="sidebar__header">
+                <img src={account.avatar} alt="avatar" />
+                <a href="#">{account.monocre}</a>
+            </div>
+
+
             {/* Category header with + button */}
             <div className="sidebar__category">
                 <h5>Organizations</h5>
                 <FontAwesomeIcon icon={['fal', 'plus']} />
             </div>
 
+            {/* List of organizations */}
             <div className="sidebar__organizations">
                 {organizations.map((org, index) => (
                     <div className="sidebar__organization" key={index}>
@@ -24,6 +36,7 @@ const OrgSidebar = ({ organizations }) => {
                 ))}
             </div>
 
+            {/* Logout button */}
             <Logout />
         </div>
     )
