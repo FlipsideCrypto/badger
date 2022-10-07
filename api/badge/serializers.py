d79b6135d6eb8149ff10627d765ed6e8bed80c5d
nftchance <chance@utc24.io>
2022-10-06 21:50:11 -0500

    fix: cleanup

--- api/badge/serializers.py
+++ api/badge/serializers.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import serializers
 
-from organization.models import Organization
-
 from .models import Badge
 
 User = get_user_model()
