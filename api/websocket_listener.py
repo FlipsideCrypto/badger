52dc87ab910b846d5030f591f341fbb9d6612a1d
nftchance <chance@utc24.io>
2022-10-07 20:35:27 -0500

    fix: basic listener

--- api/websocket_listener.py
+++ api/websocket_listener.py
@@ -14,63 +14,51 @@ users = [
     '0x8D572a89Ca4C939CDfB43F224A233c9E35e08c9C'
 ]
 
-# function reconnectingWebsocket(url) {
-#   return new Web3(
-#     new Web3.providers.WebsocketProvider(url, {
-#       timeout: 30 * 1000,
-
-#       clientConfig: {
-#         keepalive: true,
-#         keepaliveInterval: 60000,
-#       },
-
-#       reconnect: {
-#         auto: true,
-#         delay: 5000,
-#         maxAttempts: 50,
-#         onTimeout: true,
-#       },
-#     }),
-#   );
-# }
-
 mainnet = Web3.WebsocketProvider(RPC_URL)
 
-def topic(provider, address):
-    # encode the address with the mainnet provider
-    address = provider.encodeABI('address', [address])
+# def topic(provider, address):
+#     # encode the address with the mainnet provider
+#     address = provider.encodeABI('address', [address])
 
-    # create the topic
-    topic = provider.encodeABI('bytes32', [ERC20_AND_ERC721_TRANSFER])
+#     # create the topic
+#     topic = provider.encodeABI('bytes32', [ERC20_AND_ERC721_TRANSFER])
 
-    # return the topic
-    return [topic, [address]]
-
+#     # return the topic
+#     return [topic, [address]]
 
 async def main():
     print('in main')
-
-    topicsList = [
-        [ERC20_AND_ERC721_TRANSFER, {
-
-        }],
-    ]
-
     async with websockets.connect(RPC_URL) as ws:
+        print('connected')
         await ws.send(json.dumps({
             "jsonrpc": "2.0",
-            "id": 1,
             "method": "eth_subscribe",
-            "params": ["logs", {
-                "address": "0x8D572a89Ca4C939CDfB43F224A233c9E35e08c9C",
-                "topics": topicsList
-            }]
+            "params": ["newHeads"],
+            "id": 1
         }))
 
         print('sent')
         while True:
+            print('waiting')
             response = await ws.recv()
             print(response)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+# async def main():
+#     print('in main')
+#     async with websockets.connect(RPC_URL) as ws:
+#         print('connected')
+#         await ws.send(json.dumps({
+#             "jsonrpc": "2.0",
+#             "method": "eth_subscribe",
+#             "params": ["newHeads"],
+#             "id": 1
+#         }))
+
+#         print('sent')
+#         while True:
+#             print('waiting')
+#             response = await ws.recv()
+#             print(response)
