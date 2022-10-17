3e61fe7d1b07df214567206a8c2ede5fd3f221db
Mason <33527785+masonchain@users.noreply.github.com>
2022-10-16 23:27:30 -0500

    fix: error msg bug

--- frontend/src/components/Card/ErrorCard.js
+++ frontend/src/components/Card/ErrorCard.js
@@ -5,20 +5,22 @@ import "@style/Card/ErrorCard.css"
 const ErrorCard = ({ label, message, style }) => {
     return (
         <div className="error__wrapper">
-            <div className={message.length !== 0 ? "error__card active" : "error__card"} style={style}>
-                <div className="error__card__icon">
-                    <FontAwesomeIcon icon={['fal', 'fa-triangle-exclamation']} />
+            {label && message &&
+                <div className={label ? "error__card active" : "error__card"} style={style}>
+                    <div className="error__card__icon">
+                        <FontAwesomeIcon icon={['fal', 'fa-triangle-exclamation']} />
+                    </div>
+                    <div className="error__card__message">
+                        <span className="error__card__text">
+                            {label + ": " +
+                                typeof(message !== "string") ? 
+                                message['message'] || JSON.stringify(message) : 
+                                message
+                            }
+                        </span>
+                    </div>
                 </div>
-                <div className="error__card__message">
-                    <span className="error__card__text">
-                        {label + ": " +
-                            typeof(message !== "string") ? 
-                            message['message'] || JSON.stringify(message) : 
-                            message
-                        }
-                    </span>
-                </div>
-            </div>
+        }
         </div>
     )
 }
