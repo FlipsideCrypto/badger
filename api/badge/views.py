84315e220b32daa6d4571fa528104da402a173b6
nftchance <chance@utc24.io>
2022-10-18 01:35:40 -0500

    feat: pfp generator

--- api/badge/views.py
+++ api/badge/views.py
@@ -4,6 +4,7 @@ import random
 from django.contrib.auth import get_user_model
 
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import (
     IsAuthenticated,
 )
@@ -20,7 +21,26 @@ User = get_user_model()
 
 
 class ArtViewSet(viewsets.ViewSet):
-    permission_classes = [IsAuthenticated] 
+    # permission_classes = [IsAuthenticated] 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.colors = [
+            "#f06",
+            "#00FF9D",
+            "#00FFEB",
+            "#FF00EB",
+            "#FFBB00",
+            "#C668B9",
+            "#B823AF",
+            "#551CCD",
+            "#1330dd",
+            "#0fd3b2",
+            "#30E64C",
+            "#5540EA",
+            "#F19021"
+        ]
 
     def _encode(self, value):
         max_smudge = 8
@@ -46,53 +66,34 @@ class ArtViewSet(viewsets.ViewSet):
 
         return fingerprint
 
-    def list(self, request):
-        organization = request.query_params.get('organization', None)
-        organization_ethereum_address = request.query_params.get(
-            'organization_ethereum_address', None)
-        badge_name = request.query_params.get('badge_name', None)
-        invert = request.query_params.get('inverse', False)
-
-        fingerprint = self._handle_fingerprint(
-            organization_ethereum_address, badge_name)
+    @action(detail=False, methods=['get'], url_path='pfp', url_name='pfp')
+    def pfp_art(self, request):
+        char = request.query_params.get('char', None)
+        address = request.query_params.get('address', None)
 
         fill = "#fff"
-        if invert:
-            fill = "#000"
 
         size = 500
 
         blob_count = random.randint(2, 4)
         blob, useblob = "", ""
 
-        colors = [
-            "#f06",
-            "#00FF9D",
-            "#00FFEB",
-            "#FF00EB",
-            "#FFBB00",
-            "#C668B9",
-            "#B823AF",
-            "#551CCD",
-            "#1330dd",
-            "#0fd3b2",
-            "#30E64C",
-            "#5540EA",
-            "#F19021"
-        ]
+        # set seed of random
+        if address:
+            random.seed(address)
 
         # rotate blob position animation
         for i in range(blob_count):
-            x_random = random.randint(0, size / 2)
-            y_random = random.randint(0, size / 2)
+            x_random = random.randint(0, size)
+            y_random = random.randint(0, size)
 
             # create random svg blog
             x_random_rotation = random.randint(0, 360)
             y_random_rotation = random.randint(0, 360)  
 
-            random_color = random.choice(colors)
+            random_color = random.choice(self.colors)
 
-            animation_from = "f0 {x_random} {y_random}"
+            animation_from = f"0 {x_random} {y_random}"
             animation_to = f"360 {x_random} {y_random}"
 
             # random direction of rotation
@@ -126,6 +127,108 @@ class ArtViewSet(viewsets.ViewSet):
             useblob += f"""
                 <use href="#{"blob-%s" % i}" filter="url(#blur)" />
             """
+
+        text = ""
+        if char:
+            text = f"""
+                <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="250" fill="#000" font-family="'Poppins', sans-serif">
+                    {char[0]}
+                </text>
+            """
+
+        svg = f"""
+            <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" >
+                <rect width="100%" height="100%" fill="#fff" />
+
+                {blob}
+                <rect width="100%" height="100%" fill="{fill}" />
+                <filter id="blur">
+                    <feGaussianBlur in="SourceGraphic" stdDeviation="30" />
+                </filter>
+                {useblob}
+                <rect width="100%" height="100%" fill="{fill}" opacity="{0.93 if fill == "#fff" else 0.75}" />
+ 
+                {text}
+            </svg>
+        """
+
+        # return base64 encoded svg
+        image = base64.b64encode(svg.encode('utf-8')).decode('utf-8')
+
+        url_ready_base64 = f"data:image/svg+xml;base64,{image}"
+
+        return Response({
+            "image": url_ready_base64,
+        })
+
+
+    # create a custom action for badge art
+    @action(detail=False, methods=['get'], url_path='badge', url_name='badge')
+    def badge_art(self, request, pk=None):
+        organization = request.query_params.get('organization', None)
+        organization_ethereum_address = request.query_params.get(
+            'organization_ethereum_address', None)
+        badge_name = request.query_params.get('badge_name', None)
+        invert = request.query_params.get('inverse', False)
+
+        fingerprint = self._handle_fingerprint(
+            organization_ethereum_address, badge_name)
+
+        fill = "#fff"
+        if invert:
+            fill = "#000"
+
+        size = 500
+
+        blob_count = random.randint(2, 4)
+        blob, useblob = "", ""
+
+        # rotate blob position animation
+        for i in range(blob_count):
+            x_random = random.randint(0, size)
+            y_random = random.randint(0, size)
+
+            # create random svg blog
+            x_random_rotation = random.randint(0, 360)
+            y_random_rotation = random.randint(0, 360)  
+
+            random_color = random.choice(self.colors)
+
+            animation_from = f"0 {x_random} {y_random}"
+            animation_to = f"360 {x_random} {y_random}"
+
+            # random direction of rotation
+            if random.randint(0, 1) == 0:
+                animation_from = f"360 {x_random_rotation} {y_random_rotation}"
+                animation_to = f"0 {x_random_rotation} {y_random_rotation}"
+
+            rotate_blob_animation = f"""
+                <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    from="{animation_from}"
+                    to="{animation_to}"
+                    dur="{random.randint(20,45)}s"
+                    repeatCount="indefinite"
+                />
+            """
+
+            blob += f"""
+                <path 
+                    id="{"blob-%s" % i}" 
+                    d="M363.37-8.441c51.956,46.825,91.084,111.61,83.386,168.376S384.537,266.093,332.581,290.147C280.945,313.88,231.555,312.6,176.712,318.37s-115.458,18.6-168.7-5.452-98.781-84.669-89.48-135.984S-8.021,83.284,45.218,36.459C98.137-10.365,140.471-62.321,193.71-73.547S311.734-55.586,363.37-8.441Z" 
+                    transform="translate({x_random} {y_random})" 
+                    fill="{random_color}"
+                    style="mix-blend-mode: multiply;"
+                >
+                    {rotate_blob_animation}
+                </path>
+            """
+
+            useblob += f"""
+                <use href="#{"blob-%s" % i}" filter="url(#blur)" />
+            """
+
         fingerprint_svg = ""
         r = 2
         spacer = (size - (r * 2 * len(fingerprint))) / (len(fingerprint) - 1)
@@ -199,7 +302,7 @@ class ArtViewSet(viewsets.ViewSet):
                     y="{line_y + (line_height + line_buffer) * i}"
                     font-size="{line_height}"
                     fill="{text_color}"
-                    font-family="sans-serif"
+                    font-family="'Poppins', sans-serif"
                     font-weight="bold"
                 >
                     {line.upper()}
@@ -213,7 +316,7 @@ class ArtViewSet(viewsets.ViewSet):
                 dominant-baseline="middle"
                 font-size="{organization_line_height}"
                 fill="{text_color}"
-                font-family="sans-serif"
+                font-family="'Poppins', sans-serif"
                 opacity=".45"
             >
                 {organization}
@@ -221,7 +324,7 @@ class ArtViewSet(viewsets.ViewSet):
         """
 
         svg = f"""
-        <svg xmlns="http://www.w3.org/2000/svg" width="{size}" height="{size}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="{size}" height="{size}" viewbox="0 0 {size} {size}">
             <rect width="100%" height="100%" fill="#fff" />
 
             {blob}
