55910caac14f3d0f8fb5b5a218f3f0fbca1f96a8
nftchance <chance@utc24.io>
2023-01-30 10:49:58 -0600

    fix: forceful logout bug

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -9,7 +9,9 @@ const useLogout = () => {
     const { setAuthenticatedAddress } = useContext(AuthenticationContext);
 
     const logout = () => {
-        document.cookie = 'csrftoken=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
+        // TODO: Reenable this once the websocket is prevented from trying to reconnect when not enabled
+        // document.cookie = 'csrftoken=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
+
         document.cookie = 'authenticatedAddress=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
         document.cookie = 'sessionid=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
 
