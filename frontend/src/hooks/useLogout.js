b4e022de561e21d52f2bee492900d75f9a085482
nftchance <chance@utc24.io>
2023-02-11 23:25:15 -0600

    fix: undo spontaneous commit

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -12,10 +12,7 @@ const useLogout = () => {
         // TODO: Reenable this once the websocket is prevented from trying to reconnect when not enabled
         // document.cookie = 'csrftoken=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
         
-        // TODO: We cannot clear the sessionid cookie because it is an HTTP_ONLY cookie, so we need to find 
-        //       a way to clear it on the backend in order to facilitate a proper logout and switch of accounts.
-        //       We could make the sessionid be a non HTTP_ONLY cookie, but apparently it is a security risk.
-        // document.cookie = 'sessionid=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
+        document.cookie = 'sessionid=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
         document.cookie = 'authenticatedAddress=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
         setAuthenticatedAddress(null);
         navigate("/dashboard/")
