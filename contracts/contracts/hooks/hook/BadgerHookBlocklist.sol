22dc5f29f96e0fe9e8c22f027a86cd5bff8557ee
nftchance <chance@utc24.io>
2023-02-20 17:35:52 -0600

    feat: BadgerHookBlocklist

---
+++ contracts/contracts/hooks/hook/BadgerHookBlocklist.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.16;
+
+/// @dev Core dependencies.
+import {BadgerOrganizationHook} from "../BadgerOrganizationHook.sol";
+
+/**
+ * @dev Hook module that prevents certain hooks from being enabled.
+ * @author CHANCE (@nftchance)
+ * @author masonthechain (@masonthechain)
+ */
+contract BadgerHookBlocklist is BadgerOrganizationHook {
+    ////////////////////////////////////////////////////////
+    ///                      STATE                       ///
+    ////////////////////////////////////////////////////////
+
+    /// @dev Mapping of Organization to Hook to blocklisted status.
+    mapping(address => mapping(address => bool)) public blocked;
+
+    ////////////////////////////////////////////////////////
+    ///                     SETTERS                      ///
+    ////////////////////////////////////////////////////////
+
+    /**
+     * See {IBadgerHook-config}.
+     */
+    function config(bytes calldata _data) public virtual override {
+        /// @dev Decode the configuration data forwarded from the Organization.
+        address _hook = abi.decode(_data, (address));
+
+        /// @dev Forever lock away the hook from being enabled.
+        blocked[msg.sender][_hook] = true;
+    }
+
+    /**
+     * See {IBadgerHook-execute}.
+     */
+    function execute(bytes calldata _data) public virtual override {
+        /// @dev Decode the transfer data forwarded from the Organization.
+        (, address _slotHook, bool _isHook) = abi.decode(
+            _data,
+            (bytes32, address, bool)
+        );
+
+        /// @dev Enforce the hook being processed is not blocklisted.
+        /// @notice Will not run when the hook is being disabled to bypass
+        ///         a module that may be reverting or otherwise blocking.
+        require(
+            !_isHook || !blocked[msg.sender][_slotHook],
+            "BadgerHookBlacklist::execute: Cannot enable blocklisted hook."
+        );
+    }
+}
