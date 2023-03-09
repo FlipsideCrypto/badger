116fd81f569a877c3ec722f48be6df2366a19fec
Mason <33527785+masonchain@users.noreply.github.com>
2023-03-09 14:29:33 -0600

    fix: fix chainIcon

--- frontend/src/components/Icon/ChainIcon.js
+++ frontend/src/components/Icon/ChainIcon.js
@@ -3,14 +3,13 @@ import { localhost, polygon } from "@static"
 import "@style/Icon/ChainIcon.css"
 
 const icons = {
-    localhost,
-    polygon,
+    "1337": localhost,
+    "137": polygon,
 }
 
-const ChainIcon = ({ chain }) => {
-    const icon = chain && icons[chain.toLowerCase()] || '🚨'
-
-    return <img className="chainIcon" src={icon} alt={chain} />
+const ChainIcon = ({ chainId }) => {
+    const icon = icons[chainId] || '🚨'
+    return <img className="chainIcon" src={icon} alt={chainId} />
 }
 
 export { ChainIcon }
