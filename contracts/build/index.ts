9bdd08f5892c45a6011681a4cffea83c53a28b03
Mason <33527785+masonchain@users.noreply.github.com>
2023-03-16 18:14:11 -0500

    feat: transferBound in package
    (did not manage to fix the package like we wanted and said no more wasting time with it)

--- contracts/build/index.ts
+++ contracts/build/index.ts
@@ -1,7 +1,7 @@
 export const BADGER_DEPLOYMENTS = {
     "137": { 
-        badger: "0x19D97D8fA813EE2f51aD4B4e04EA08bAf4DFfC28",
-        transferBoundHook: ""
+        badger: "0x72b03C649953CA95B920f60A5687e4d2DACf45c0",
+        transferBoundHook: "0xd66c862d84389A96E2E84644243582c0f438e46F"
     },
     "1337": {
         badger: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
