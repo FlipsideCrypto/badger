4c307e2278c708539161668fbe52304fd1dd79eb
nftchance <chance@utc24.io>
2023-03-24 15:24:12 -0500

    feat: go home on logout

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -3,11 +3,10 @@ import { useNavigate } from "react-router-dom";
 const useLogout = () => {
     const navigate = useNavigate();
 
-
     const logout = () => {
         document.cookie = 'sessionid=; Path=/; Expires=Sat, 01 Jan 2000 00:00:001 GMT;';
 
-        navigate("/dashboard/")
+        navigate("/")
     }
 
     return { logout }
