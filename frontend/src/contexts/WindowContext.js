33f8f5697f5423ecef1b7fddb547f7c0f026e021
Mason <33527785+masonchain@users.noreply.github.com>
2023-03-25 21:46:17 -0500

    fix: unused hook

--- frontend/src/contexts/WindowContext.js
+++ frontend/src/contexts/WindowContext.js
@@ -4,19 +4,15 @@ import { useTransactionWindow } from "@hooks";
 const WindowContext = createContext();
 
 const useErrorModal = () => {}
-const useMessageWindow = () => {}
 
 const WindowContextProvider = ({ children }) => {
     const errorModal = useErrorModal();
-
-    const messageWindow = useMessageWindow();
 
     const transactionWindow = useTransactionWindow();
 
     return (
         <WindowContext.Provider value={{
             errorModal,
-            messageWindow,
             transactionWindow
         }}>
             {children}
