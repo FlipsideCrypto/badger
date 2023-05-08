d2e9740ef4f2e8af7a71b17d0cff59b3d3462f5d
sftchance <chance+sft@utc24.io>
2023-05-08 14:31:36 -0500

    fix: discovery cleanup

--- frontend/src/pages/Discover/Discover.js
+++ frontend/src/pages/Discover/Discover.js
@@ -1,36 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { ActionButton, Header, Input, OrgTable, SEO } from "@components";
+import { ActionButton, Header, Input, OrgTable, SEO } from '@components';
 
-import { useDebounce, useNavigateAddress } from "@hooks";
+import { useDebounce, useNavigateAddress } from '@hooks';
 
 const Discover = () => {
     const navigate = useNavigateAddress();
 
     const [organizations, setOrganizations] = useState(null);
-    const [search, setSearch] = useState("");
+    const [search, setSearch] = useState('');
 
     const debouncedSearch = useDebounce(search, 300);
 
     const onSearchChange = (e) => {
         setSearch(e.target.value);
-    }
+    };
 
     useEffect(() => {
         const getOrganizations = () => {
-            const url = `http://localhost:8000/organizations/?search=${debouncedSearch}`
+            const url = `http://localhost:8000/organizations/?search=${debouncedSearch}`;
 
             fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    setOrganizations(data)
-                })
-        }
+                .then((response) => response.json())
+                .then((data) => {
+                    setOrganizations(data);
+                });
+        };
 
-        getOrganizations()
-    }, [debouncedSearch])
+        getOrganizations();
+    }, [debouncedSearch]);
 
     return (
         <>
@@ -39,60 +39,69 @@ const Discover = () => {
             <div className="dashboard">
                 <div className="dashboard__contents">
                     <div className="dashboard__content">
-                        <Header back={() => navigate("/dashboard/")} />
+                        <Header back={() => navigate('/dashboard/')} />
 
-                        <div style={{
-                            display: "grid",
-                            gridTemplateColumns: "auto auto",
-                            alignItems: "center",
-                        }}>
+                        <div
+                            style={{
+                                display: 'grid',
+                                gridTemplateColumns: 'auto auto',
+                                alignItems: 'center',
+                            }}
+                        >
                             <h2>Discover Organizations</h2>
 
-                            <p style={{ textAlign: "right" }}>{organizations && organizations.length || 0} results</p>
+                            <p style={{ textAlign: 'right' }}>{(organizations && organizations.length) || 0} results</p>
                         </div>
 
-                        <div style={{
-                            display: "grid",
-                            gridTemplateColumns: "auto min-content",
-                            alignItems: "center",
-                            gap: "10px"
-                        }}>
-                            <div style={{
-                                display: "flex",
-                                alignItems: "center",
-                            }}>
+                        <div
+                            style={{
+                                display: 'grid',
+                                gridTemplateColumns: 'auto',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                }}
+                            >
                                 <Input
                                     placeholder="Search..."
                                     value={search}
                                     onChange={onSearchChange}
                                     style={{
                                         borderTopRightRadius: 0,
-                                        borderBottomRightRadius: 0
+                                        borderBottomRightRadius: 0,
                                     }}
                                     append={
                                         <button
                                             className="primary"
-                                            onClick={() => navigate("/dashboard/organization/new/")}>
-                                            <span><FontAwesomeIcon icon={['fal', 'search']} /></span>
+                                            onClick={() => navigate('/dashboard/organization/new/')}
+                                        >
+                                            <span>
+                                                <FontAwesomeIcon icon={['fal', 'search']} />
+                                            </span>
                                         </button>
-                                    } />
+                                    }
+                                />
                             </div>
 
-                            <div className="form__group input__group">
+                            {/* <div className="form__group input__group">
                                 <ActionButton
                                     className="secondary"
                                     beforeText="All"
                                     icon={['fal', 'sort']}
                                     onClick={() => navigate("/dashboard/organization/new/")} />
-                            </div>
+                            </div> */}
                         </div>
                     </div>
 
                     {organizations && organizations.length > 0 && <OrgTable organizations={organizations} />}
                 </div>
-            </div >
+            </div>
         </>
-    )
-}
+    );
+};
 
-export { Discover }
+export { Discover };
