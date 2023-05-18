e928f01138641713f3a93d014a6c6bc5ef050c63
Mason <33527785+masonchain@users.noreply.github.com>
2023-05-18 14:11:57 -0500

    fix: close navbar on click out

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-import { LogoIcon } from "@components";
+import { LogoIcon } from '@components';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { HOME_LINKS } from "@static";
+import { HOME_LINKS } from '@static';
 
-import "@style/Navbar/Navbar.css";
+import '@style/Navbar/Navbar.css';
 
 const Navbar = () => {
     const [isFilled, setIsFilled] = useState(false);
@@ -17,34 +17,45 @@ const Navbar = () => {
     useEffect(() => {
         const handleScroll = () => setIsFilled(window.scrollY > 75);
 
-        window.addEventListener("scroll", handleScroll);
+        window.addEventListener('scroll', handleScroll);
 
-        return () => window.removeEventListener("scroll", handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
     }, []);
+
+    useEffect(() => {
+        const handleNavClick = (e) => {
+            if (!e.target.closest('.navbar')) setIsMenuOpen(false);
+        };
+
+        if (!isMenuOpen) return () => window.removeEventListener('pointerdown', handleNavClick);
+
+        window.addEventListener('pointerdown', (e) => handleNavClick(e));
+    }, [isMenuOpen]);
 
     const onHamburgerClick = () => {
         setIsMenuOpen(!isMenuOpen);
-    }
+    };
 
     return (
-        <div className={`navbar ${(isMenuOpen || isFilled) && "filled"}`}>
+        <div className={`navbar ${(isMenuOpen || isFilled) && 'filled'}`}>
             <div className="container">
-                <Link to="/"><LogoIcon /></Link>
+                <Link to="/">
+                    <LogoIcon />
+                </Link>
 
-                <div className={`links ${isMenuOpen && "active"}`}>
+                <div className={`links ${isMenuOpen && 'active'}`}>
                     <div className="navbar__links__left">
                         <Link to="/stories/">Customer Stories</Link>
                         <Link to="/faq/">FAQ</Link>
-                        <a href={HOME_LINKS.gitbook} target="_blank" rel="noreferrer">Docs</a>
+                        <a href={HOME_LINKS.gitbook} target="_blank" rel="noreferrer">
+                            Docs
+                        </a>
                     </div>
 
                     <div className="navbar__links__right">
                         <Link className="enter" to="/dashboard/">
-                            <button className="secondary">
-                                Enter app
-                            </button>
+                            <button className="secondary">Enter app</button>
                         </Link>
-
                     </div>
                 </div>
                 <div className="hamburger">
@@ -54,7 +65,7 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export { Navbar }
+export { Navbar };
